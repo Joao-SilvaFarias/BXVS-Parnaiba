9de2d422a3bd22187c25d283a25ae66379137aa9
João Victor Silva Farias <joaovictorsilvafarias15@gmail.com>
2025-10-19 23:00:05 -0300

    Teste

--- src/components/Matricula/BiometriaFacial.js
+++ src/components/Matricula/BiometriaFacial.js
@@ -328,7 +328,7 @@ export default function BiometriaFacial({ cliente, setCliente, setBiometria }) {
                 {cameraAtiva ? (
                     <>
                         <div className={styles.tituloContainer}>
-                            <p className={styles.txtEtapa}>3° Etapa de Verificação</p>
+                            <p className={styles.txtEtapa} onClick={() => alert(cliente.email)}>3° Etapa de Verificação</p>
                             <h2 className={styles.txtBiometriaFacial}>
                                 Biometria Facial <span className={styles.txtFoto}>{"<"} Liveness</span> {/* Mudança de Foto para Liveness */}
                             </h2>
