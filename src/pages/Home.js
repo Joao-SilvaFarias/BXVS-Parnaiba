bed6a68d6d287dac43bd7aa87974d436b9f0baa1
João Victor Silva Farias <joaovictorsilvafarias15@gmail.com>
2025-10-21 21:59:56 -0300

    Teste

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,10 +13,10 @@ export default function Home(props) {
             const res = await axios.get("https://joaofarias16.pythonanywhere.com/buscarCliente", {params: {id: props.cliente.idCliente}});
             if(res.data){
                 setCadastroRosto(res.data.face_embedding);
-                alert(res.data.face_embedding);
             }
         }
-    }, []);
+        buscarCliente();
+    }, [props.cliente.idCliente]);
 
     return (
         <>
