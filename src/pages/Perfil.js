5f3776c7b938fda372b0aea51b144237da89f2ce
João Victor Silva Farias <joaovictorsilvafarias15@gmail.com>
2025-10-24 00:15:22 -0300

    Teste

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -11,17 +11,17 @@ export default function Perfil({ cliente, setCliente }) {
     const nomeRef = useRef(null);
     const toggleEdit = () => {
         setSoLer(prev => {
-        const novoEstado = !prev;
-        if (novoEstado === false) {
-            // Espera o input liberar o readOnly e então foca
-            setTimeout(() => nomeRef.current?.focus(), 0);
-        }
-        return novoEstado;
-    });
+            const novoEstado = !prev;
+            if (novoEstado === false) {
+                // Espera o input liberar o readOnly e então foca
+                setTimeout(() => nomeRef.current?.focus(), 0);
+            }
+            return novoEstado;
+        });
     };
 
     useEffect(() => {
-        if(cliente){
+        if (cliente) {
             setClienteOriginal(cliente);
         }
     }, [cliente]);
@@ -53,89 +53,90 @@ export default function Perfil({ cliente, setCliente }) {
     return (
         <>
             <Header cliente={cliente} setCliente={setCliente} />
-            <div className={styles.container}>
-                <div className={styles.cardMatricula}>
-                    <div className={styles.headerCardMatricula}>
-                        <p className={styles.tituloMatricula}>Minha matrícula</p>
-                        {soLer ?
-                            <button onClick={toggleEdit} className={styles.btnInformacoes}>Editar informações</button> :
-                            <form onSubmit={handleSubmit}>
-                                <button type="submit" className={styles.btnSalvar}>Confirmar</button>
-                            </form>}
-                    </div>
-                    <hr className={styles.hr}></hr>
-                    <div className={styles.containerPerfil}>
-                        <img className={styles.imgPerfil} alt="Perfil" src="/img/iconUser.png" />
-                        <p className={styles.nomePerfil}>{cliente.nome}</p>
-                    </div>
-                    <div className={styles.containerDados}>
-                        <div className={styles.containerInputs}>
-                            <div className={styles.containerInput}>
-                                <label>Nome completo</label>
-                                <input type="text" name="nome" ref={nomeRef} className={styles.inputPerfil} value={cliente.nome} readOnly={soLer} onChange={handleChange}/>
-                            </div>
-                            <div className={styles.containerInput}>
-                                <label>CPF</label>
-                                <input type="text" name="cpf" className={styles.inputPerfil} value={cliente.cpf} readOnly={soLer} onChange={handleChange} />
-                            </div>
-                            <div className={styles.containerInput}>
-                                <label>Contato de emergência</label>
-                                <input type="text" name="telefone" className={styles.inputPerfil} value={cliente.telefone} readOnly={soLer} onChange={handleChange} />
-                            </div>
+            {cliente &&
+                <div className={styles.container}>
+                    <div className={styles.cardMatricula}>
+                        <div className={styles.headerCardMatricula}>
+                            <p className={styles.tituloMatricula}>Minha matrícula</p>
+                            {soLer ?
+                                <button onClick={toggleEdit} className={styles.btnInformacoes}>Editar informações</button> :
+                                <form onSubmit={handleSubmit}>
+                                    <button type="submit" className={styles.btnSalvar}>Confirmar</button>
+                                </form>}
                         </div>
-                        <div className={styles.containerInputs}>
-                            <div className={styles.containerInput}>
-                                <label>Data de nascimento</label>
-                                <input type="date" name="dataNascimento" className={styles.inputPerfil} value={cliente.dataNascimento} readOnly={soLer} onChange={handleChange} />
-                            </div>
-                            <div className={styles.containerInput}>
-                                <label>RG</label>
-                                <input type="text" name="rg" className={styles.inputPerfil} value={cliente.rg} readOnly={soLer} onChange={handleChange} />
-                            </div>
-                            <div className={styles.containerInput}>
-                                <label>Email</label>
-                                <input type="text" name="email" className={styles.inputPerfil} value={cliente.email} readOnly={soLer} onChange={handleChange} />
-                            </div>
+                        <hr className={styles.hr}></hr>
+                        <div className={styles.containerPerfil}>
+                            <img className={styles.imgPerfil} alt="Perfil" src="/img/iconUser.png" />
+                            <p className={styles.nomePerfil}>{cliente.nome}</p>
                         </div>
-                        <div className={styles.containerInputs}>
-                            <div className={styles.containerInput}>
-                                <label>Sexo</label>
-                                <input type="text" name="sexo" className={styles.inputPerfil} value={cliente.sexo} readOnly={soLer} onChange={handleChange} />
-                            </div>
-                            <div className={styles.containerInput}>
-                                <label>Telefone</label>
-                                <input type="text" name="telefone" className={styles.inputPerfil} value={cliente.telefone} readOnly={soLer} onChange={handleChange} />
-                            </div>
-                            <div className={styles.containerInput}>
-                                <label>Endereço completo</label>
-                                <input type="text" name="endereco" className={styles.inputPerfil} value={cliente.endereco} readOnly={soLer} onChange={handleChange} />
-                            </div>
-                        </div>
-                        <div className={styles.containerCardPlano}>
-                            <label>Meu plano</label>
-                            <div className={styles.cardPlano}>
-                                <div className={styles.textPlano}>Plano PRO</div>
-                                <p className={styles.textPlano}>Pensado para quem quer performance máxima. Acesso total às aulas, suporte exclusivo dos coaches e prioridade em eventos e desafios da comunidade BXVS.</p>
-                                <div className={styles.vencimentoContainer}>
-                                    <p className={styles.textPlano}>Vence em:</p>
-                                    <p className={styles.vencimento}>10/12/2025</p>
+                        <div className={styles.containerDados}>
+                            <div className={styles.containerInputs}>
+                                <div className={styles.containerInput}>
+                                    <label>Nome completo</label>
+                                    <input type="text" name="nome" ref={nomeRef} className={styles.inputPerfil} value={cliente.nome} readOnly={soLer} onChange={handleChange} />
                                 </div>
-                                <hr className={styles.hr}></hr>
-                                <div className={styles.containerPagamento}>
-                                    <div className={styles.containerValor}>
-                                        <p className={styles.textPlano}>Valor:</p>
-                                        <p className={styles.textPlano}>R$ 150,00</p>
+                                <div className={styles.containerInput}>
+                                    <label>CPF</label>
+                                    <input type="text" name="cpf" className={styles.inputPerfil} value={cliente.cpf} readOnly={soLer} onChange={handleChange} />
+                                </div>
+                                <div className={styles.containerInput}>
+                                    <label>Contato de emergência</label>
+                                    <input type="text" name="telefone" className={styles.inputPerfil} value={cliente.telefone} readOnly={soLer} onChange={handleChange} />
+                                </div>
+                            </div>
+                            <div className={styles.containerInputs}>
+                                <div className={styles.containerInput}>
+                                    <label>Data de nascimento</label>
+                                    <input type="date" name="dataNascimento" className={styles.inputPerfil} value={cliente.dataNascimento} readOnly={soLer} onChange={handleChange} />
+                                </div>
+                                <div className={styles.containerInput}>
+                                    <label>RG</label>
+                                    <input type="text" name="rg" className={styles.inputPerfil} value={cliente.rg} readOnly={soLer} onChange={handleChange} />
+                                </div>
+                                <div className={styles.containerInput}>
+                                    <label>Email</label>
+                                    <input type="text" name="email" className={styles.inputPerfil} value={cliente.email} readOnly={soLer} onChange={handleChange} />
+                                </div>
+                            </div>
+                            <div className={styles.containerInputs}>
+                                <div className={styles.containerInput}>
+                                    <label>Sexo</label>
+                                    <input type="text" name="sexo" className={styles.inputPerfil} value={cliente.sexo} readOnly={soLer} onChange={handleChange} />
+                                </div>
+                                <div className={styles.containerInput}>
+                                    <label>Telefone</label>
+                                    <input type="text" name="telefone" className={styles.inputPerfil} value={cliente.telefone} readOnly={soLer} onChange={handleChange} />
+                                </div>
+                                <div className={styles.containerInput}>
+                                    <label>Endereço completo</label>
+                                    <input type="text" name="endereco" className={styles.inputPerfil} value={cliente.endereco} readOnly={soLer} onChange={handleChange} />
+                                </div>
+                            </div>
+                            <div className={styles.containerCardPlano}>
+                                <label>Meu plano</label>
+                                <div className={styles.cardPlano}>
+                                    <div className={styles.textPlano}>Plano PRO</div>
+                                    <p className={styles.textPlano}>Pensado para quem quer performance máxima. Acesso total às aulas, suporte exclusivo dos coaches e prioridade em eventos e desafios da comunidade BXVS.</p>
+                                    <div className={styles.vencimentoContainer}>
+                                        <p className={styles.textPlano}>Vence em:</p>
+                                        <p className={styles.vencimento}>10/12/2025</p>
                                     </div>
-                                    <button className={styles.btnMercadoPago}>
-                                        <img alt="Mercado pago" src="/img/mercadoPagoIcon.png" className={styles.imgMercadoPago} />
-                                        <p>Realizar pagamento</p>
-                                    </button>
+                                    <hr className={styles.hr}></hr>
+                                    <div className={styles.containerPagamento}>
+                                        <div className={styles.containerValor}>
+                                            <p className={styles.textPlano}>Valor:</p>
+                                            <p className={styles.textPlano}>R$ 150,00</p>
+                                        </div>
+                                        <button className={styles.btnMercadoPago}>
+                                            <img alt="Mercado pago" src="/img/mercadoPagoIcon.png" className={styles.imgMercadoPago} />
+                                            <p>Realizar pagamento</p>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </div>}
             <Footer />
         </>
     )
