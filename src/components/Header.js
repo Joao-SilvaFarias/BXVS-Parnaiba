167a03b4e94b944b254a85ca5b74f20b38165542
João Victor Silva Farias <joaovictorsilvafarias15@gmail.com>
2025-10-24 07:18:04 -0300

    Teste

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,6 +45,7 @@ export default function Header({ cliente, setCliente }) {
                         <p className={`${styles.statusMatricula} ${cliente && styles.verde}`}>matriculado</p>
                     </div>
                     <img src="/img/iconUser.png" alt="Ícone de usuário" className={styles.iconUser} />
+                    <button onClick={logout}>Logout</button>
                 </div>
             ) : (
                 <div className={styles.usuarioContainer}>
